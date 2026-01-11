563f1fa11fbc79f65027f4567ef0fde5ff2b2540
Kass <kass.grzelak@proton.me>
2026-01-11 18:29:06 +0000

    arithmetic instructions implemented

--- VM.cpp
+++ VM.cpp
@@ -4,6 +4,7 @@
 
 #include "VM.hpp"
 
+#include <complex>
 #include <cstdio>
 
 #include "debug.hpp"
@@ -19,14 +20,31 @@ InterpretResult VM::interpret(const Chunk* chunk)
 	return run();
 }
 
+static size_t readByte24(const uint8_t* ip)
+{
+	const uint8_t byte1 = *ip++ << 16;
+	const uint8_t byte2 = *ip++ << 8;
+	const uint8_t byte3 = *ip++;
 
+	return byte1 | byte2 | byte3;
+}
 
 InterpretResult VM::run()
 {
 	const uint8_t* ip = chunk->getCodePointer();
 
 #define READ_BYTE() (*ip++)
+#define READ_BYTE_24() (readByte24(ip))
 #define READ_CONSTANT() (chunk->getConstant(READ_BYTE()))
+#define READ_CONSTANT_24() (chunk->getConstant(READ_BYTE_24()))
+#define BINARY_OP(op) \
+	do \
+	{ \
+		const double b = pop(); \
+		const double a = pop(); \
+		push(a op b); \
+	} \
+	while (false)
 
 	for (;;)
 	{
@@ -49,6 +67,26 @@ InterpretResult VM::run()
 				push(constant);
 				break;
 			}
+			case OP_CONSTANT_24:
+			{
+				const Value constant = READ_CONSTANT_24();
+				push(constant);
+				break;
+			}
+
+			case OP_NEGATE: stack.back() *= -1; break;
+
+			case OP_ADD: BINARY_OP(+); break;
+			case OP_SUBTRACT: BINARY_OP(-); break;
+			case OP_MULTIPLY: BINARY_OP(*); break;
+			case OP_DIVIDE: BINARY_OP(/); break;
+			case OP_EXPONENT:
+			{
+				const double y = pop();
+				const double x = pop();
+				push(std::pow(x, y));
+				break;
+			}
 
 			case OP_RETURN:
 			{
@@ -63,7 +101,10 @@ InterpretResult VM::run()
 	}
 
 #undef READ_BYTE
+#undef READ_BYTE_24
 #undef READ_CONSTANT
+#undef READ_CONSTANT_24
+#undef BINARY_OP
 }
 
 void VM::push(const Value& value) noexcept
--- main.cpp
+++ main.cpp
@@ -8,8 +8,18 @@ int main(int argc, const char* argv[])
 
 	Chunk chunk;
 
-	chunk.writeConstant(1.2, 1);
-	chunk.write(OP_RETURN, 1);
+	// for (int i = 0; i < 259; ++i)
+	// {
+	// 	chunk.writeConstant(1.2, 1);
+	// }
+
+	chunk.writeConstant(4, 1);
+	chunk.writeConstant(1, 1);
+	chunk.write(OP_SUBTRACT, 1);
+	chunk.writeConstant(2, 1);
+	chunk.write(OP_MULTIPLY, 1);
+
+	chunk.write(OP_RETURN, 2);
 
 	vm.interpret(&chunk);
 
